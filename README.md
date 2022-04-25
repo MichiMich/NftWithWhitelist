c2e5c05282fac5f049abc36b308f73527c2a61db
MichiMich <michimich054@gmail.com>
2022-04-25 12:31:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Access control
+
+## use case
+Whitelist/Contract access limitation:
+Usable for granting access only to previously added addresses
+
+## dependencies
+### openzeppelin
+    ´npm install --save-dev @openzeppelin/contracts´
+    
+
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
---
+++ scripts/deploy_AsciiFaces.js
@@ -0,0 +1,47 @@
+const hre = require("hardhat");
+const { ethers } = require("hardhat");
+
+async function main() {
+
+
+    // We get the contract to deploy
+    const NftContract = await hre.ethers.getContractFactory("OnChainNftMintContract");
+    const nftContract = await NftContract.deploy(false, ethers.utils.parseUnits("1", 15)); //mint price set to 1e15 = 1 finney = 0.001 eth
+
+    await nftContract.deployed();
+
+    console.log("nftContract deployed to:", nftContract.address);
+
+    //Check how much mint combinations are available
+    let availableMintCombinations = await nftContract.totalSupply();
+    console.log("total nft supply: ", availableMintCombinations);
+
+    //now lets mint out
+    let ethTransmitValueInWei = 1e15;
+    console.log("contract balance before mint: ", await nftContract.getBalance());
+    for (let i = 0; i < availableMintCombinations; i++) {
+        createdNft = await nftContract.mint({ value: ethTransmitValueInWei });
+        console.log("created nft: ", createdNft);
+
+
+        //tokenUri
+        let tokenUri = await nftContract.tokenURI(i);
+        console.log("created AsciiFace: ", tokenUri);
+
+    }
+
+    console.log("contract balance after mint: ", await nftContract.getBalance());
+    dfadfa
+
+
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
